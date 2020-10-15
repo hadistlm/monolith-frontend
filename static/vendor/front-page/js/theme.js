be85bdcbbc8690f946dbec8080e446b5d8455222
Islaa <hadist.lm@gmail.com>
2020-10-15 17:22:42 +0700

    Adding : assets required for building the view

---
+++ static/vendor/front-page/js/theme.js
@@ -0,0 +1,11 @@
+(function ($) {
+	"use strict";
+
+// Preloader
+$(window).on('load', function () {
+	$('.lds-ellipsis').fadeOut(); // will first fade out the loading animation
+	$('.preloader').delay(333).fadeOut('slow'); // will fade out the white DIV that covers the website.
+	$('body').delay(333);
+});
+
+})(jQuery)
